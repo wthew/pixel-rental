4e48e3792acafebf41efd817d534c426a9d16630
well <the.w.3107@gmail.com>
2025-01-15 16:09:06 -0300

    chore: runnable code

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,12 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App to start working on your app!</Text>
+      <Link href='/test'>test</Link>
       <StatusBar style="auto" />
     </View>
   );
